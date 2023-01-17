fd815c8b11367d6db2913bb38c4a3cdcf048fe1d
frankieali <frankieali4@gmail.com>
2023-01-16 21:08:34 -0500

    add roadmap post

--- terms.js
+++ terms.js
@@ -1,5 +1,6 @@
 module.exports = {
   ttrpg: "Table Top Role Playing Game",
   systemname: "All themes have a 'pretty' name and a camel cased 'system' name",
-  pbr: "<b>P</b>hysics-<b>B</b>ased <b>R</b>endering - is a virtual material pipeline that can simulate any kind of physical material."
-}
+  pbr: "<b>P</b>hysics-<b>B</b>ased <b>R</b>endering - is a virtual material pipeline that can simulate any kind of physical material.",
+  ogl: "Open Game License",
+};
